026e6eeb1660a4f8de6e3ba9e16a5e1cd7b656d2
rkrish00568 <143256126+rkrish00568@users.noreply.github.com>
2025-07-25 01:35:32 +0530

    Update traffic_test.v
    updated

--- traffic_test.v
+++ traffic_test.v
@@ -1,23 +1,42 @@
-`timescale 1ns/1ps
-module traffic_control_tb;
+`timescale 1ns / 1ps
 
-wire [2:0] n_lights,s_lights,e_lights,w_lights;
-reg clk,rst_a;
+module traffic_light_tb;
 
-traffic_control DUT (n_lights,s_lights,e_lights,w_lights,clk,rst_a);
+    reg clk;
+    reg rst;
 
-initial
- begin
-  clk=1'b1;
-  forever #5 clk=~clk;
- end
- 
-initial
- begin
-  rst_a=1'b1;
-  #15;
-  rst_a=1'b0;
-  #1000;
-  $stop;
- end
+    wire [2:0] light_M1;
+    wire [2:0] light_S;
+    wire [2:0] light_MT;
+    wire [2:0] light_M2;
+
+    Traffic_Light_Controller uut (
+        .clk(clk),
+        .rst(rst),
+        .light_M1(light_M1),
+        .light_S(light_S),
+        .light_MT(light_MT),
+        .light_M2(light_M2)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        rst = 1;
+
+        $dumpfile("traffic_light.vcd");
+        $dumpvars(0, traffic_light_tb);
+
+        $display("Time\tState\tM1\tM2\tMT\tS");
+        $monitor("%0t\t%b\t%b\t%b\t%b\t%b", $time, uut.ps, light_M1, light_M2, light_MT, light_S);
+
+        #15;
+        rst = 0;
+
+        #300;
+
+        $finish;
+    end
+
 endmodule
