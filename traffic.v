9f92ac45a7e1fb1558da079e41431d91d827ca10
rkrish00568 <143256126+rkrish00568@users.noreply.github.com>
2025-07-25 01:34:45 +0530

    Update traffic.v
    updated

--- traffic.v
+++ traffic.v
@@ -1,215 +1,159 @@
-module traffic_control(n_lights,s_lights,e_lights,w_lights,clk,rst_a);
+module Traffic_Light_Controller(
 
-   output reg [2:0] n_lights,s_lights,e_lights,w_lights; 
-   input      clk;
-   input      rst_a;
- 
-   reg [2:0] state;
- 
-   parameter [2:0] north=3'b000;
-   parameter [2:0] north_y=3'b001;
-   parameter [2:0] south=3'b010;
-   parameter [2:0] south_y=3'b011;
-   parameter [2:0] east=3'b100;
-   parameter [2:0] east_y=3'b101;
-   parameter [2:0] west=3'b110;
-   parameter [2:0] west_y=3'b111;
 
-   reg [2:0] count;
- 
+    input clk,rst,
+    output reg [2:0]light_M1,
+    output reg [2:0]light_S,
+    output reg [2:0]light_MT,
+    output reg [2:0]light_M2
+    );
+    
+    parameter  S1=0, S2=1, S3 =2, S4=3, S5=4,S6=5;
+    reg [3:0]count;
+    reg[2:0] ps;
+    parameter  sec7=7,sec5=5,sec2=2,sec3=3;
 
-   always @(posedge clk, posedge rst_a)
-     begin
-        if (rst_a)
-            begin
-                state=north;
-                count =3'b000;
-            end
+   
+    
+    always@(posedge clk or posedge rst)
+        begin
+        if(rst==1)
+        begin
+        ps<=S1;
+        count<=0;
+        end
         else
+        
+      
+       
+            
+            case(ps)
+                S1: if(count<sec7)
+                        begin
+                        ps<=S1;
+                        count<=count+1;
+                        end
+                    else
+                        begin
+                        ps<=S2;
+                        count<=0;
+                        end
+                S2: if(count<sec2)
+                        begin
+                        ps<=S2;
+                        count<=count+1;
+                        end
+
+                    else
+                        begin
+                        ps<=S3;
+                        count<=0;
+                        end
+                S3: if(count<sec5)
+                        begin
+                        ps<=S3;
+                        count<=count+1;
+                        end
+
+                    else
+                        begin
+                        ps<=S4;
+                        count<=0;
+                        end
+                S4:if(count<sec2)
+                        begin
+                        ps<=S4;
+                        count<=count+1;
+                        end
+
+                    else
+                        begin
+                        ps<=S5;
+                        count<=0;
+                        end
+                S5:if(count<sec3)
+                        begin
+                        ps<=S5;
+                        count<=count+1;
+                        end
+
+                    else
+                        begin
+                        ps<=S6;
+                        count<=0;
+                        end
+
+                S6:if(count<sec2)
+                        begin
+                        ps<=S6;
+                        count<=count+1;
+                        end
+
+                    else
+                        begin
+                        ps<=S1;
+                        count<=0;
+                        end
+                default: ps<=S1;
+                endcase
+            end   
+
+            always@(ps)    
             begin
-                case (state)
-                north :
+                
+                case(ps)
+                     
+                    S1:
                     begin
-                        if (count==3'b111)
-                            begin
-                            count=3'b000;
-                            state=north_y;
-                            end
-                        else
-                            begin
-                            count=count+3'b001;
-                            state=north;
-                            end
+                       light_M1<=3'b001;
+                       light_M2<=3'b001;
+                       light_MT<=3'b100;
+                       light_S<=3'b100;
                     end
-
-                north_y :
+                    S2:
+                    begin 
+                       light_M1<=3'b001;
+                       light_M2<=3'b010;
+                       light_MT<=3'b100;
+                       light_S<=3'b100;
+                    end
+                    S3:
                     begin
-                        if (count==3'b011)
-                            begin
-                            count=3'b000;
-                            state=south;
-                            end
-                        else
-                            begin
-                            count=count+3'b001;
-                            state=north_y;
-                        end
+                       light_M1<=3'b001;
+                       light_M2<=3'b100;
+                       light_MT<=3'b001;
+                       light_S<=3'b100;
                     end
-
-               south :
+                    S4:
                     begin
-                        if (count==3'b111)
-                            begin
-                            count=3'b0;
-                            state=south_y;
-                            end
-                        else
-                            begin
-                            count=count+3'b001;
-                            state=south;
-                        end
+                       light_M1<=3'b010;
+                       light_M2<=3'b100;
+                       light_MT<=3'b010;
+                       light_S<=3'b100;
                     end
-
-            south_y :
-                begin
-                    if (count==3'b011)
-                        begin
-                        count=3'b0;
-                        state=east;
-                        end
-                    else
-                        begin
-                        count=count+3'b001;
-                        state=south_y;
-                        end
+                    S5:
+                    begin
+                       light_M1<=3'b100;
+                       light_M2<=3'b100;
+                       light_MT<=3'b100;
+                       light_S<=3'b001;
                     end
-
-            east :
-                begin
-                    if (count==3'b111)
-                        begin
-                        count=3'b0;
-                        state=east_y;
-                        end
-                    else
-                        begin
-                        count=count+3'b001;
-                        state=east;
-                        end
+                    S6:
+                    begin 
+                       light_M1<=3'b100;
+                       light_M2<=3'b100;
+                       light_MT<=3'b100;
+                       light_S<=3'b010;
                     end
-
-            east_y :
-                begin
-                    if (count==3'b011)
-                        begin
-                        count=3'b0;
-                        state=west;
-                        end
-                    else
-                        begin
-                        count=count+3'b001;
-                        state=east_y;
-                        end
+                    default:
+                    begin 
+                       light_M1<=3'b000;
+                       light_M2<=3'b000;
+                       light_MT<=3'b000;
+                       light_S<=3'b000;
                     end
+                    endcase
+            end                
+              
 
-            west :
-                begin
-                    if (count==3'b111)
-                        begin
-                        state=west_y;
-                        count=3'b0;
-                        end
-                    else
-                        begin
-                        count=count+3'b001;
-                        state=west;
-                        end
-                    end
-
-            west_y :
-                begin
-                    if (count==3'b011)
-                        begin
-                        state=north;
-                        count=3'b0;
-                        end
-                    else
-                        begin
-                        count=count+3'b001;
-                        state=west_y;
-                        end
-                    end
-            endcase // case (state)
-        end // always @ (state)
-    end 
-
-
-always @(state)
-     begin
-         case (state)
-            north :
-                begin
-                    n_lights = 3'b001;
-                    s_lights = 3'b100;
-                    e_lights = 3'b100;
-                    w_lights = 3'b100;
-                end // case: north
-
-            north_y :
-                begin
-                    n_lights = 3'b010;
-                    s_lights = 3'b100;
-                    e_lights = 3'b100;
-                    w_lights = 3'b100;
-                end // case: north_y
-
-            south :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b001;
-                    e_lights = 3'b100;
-                    w_lights = 3'b100;
-                end // case: south
-
-            south_y :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b010;
-                    e_lights = 3'b100;
-                    w_lights = 3'b100;
-                end // case: south_y
-
-            west :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b100;
-                    e_lights = 3'b100;
-                    w_lights = 3'b001;
-                end // case: west
-
-            west_y :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b100;
-                    e_lights = 3'b100;
-                    w_lights = 3'b010;
-                end // case: west_y
-
-            east :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b100;
-                    e_lights = 3'b001;
-                    w_lights = 3'b100;
-                end // case: east
-
-            east_y :
-                begin
-                    n_lights = 3'b100;
-                    s_lights = 3'b100;
-                    e_lights = 3'b010;
-                    w_lights = 3'b100;
-                end // case: east_y
-            endcase // case (state)
-     end // always @ (state)
 endmodule
